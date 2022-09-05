f7f8ea074f4a9321642cefb57323dc3f7d5c9701
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-09-05 18:14:46 +0200

    Classes: cart setup

--- classes/cart.php
+++ classes/cart.php
@@ -1,5 +1,14 @@
 <?php
 
+require_once __DIR__ . '/../inclusions.php';
+
 class Cart{
-    
+    protected $cartUser;
+    protected $associatedCreditCards = [];
+
+    public function __construct($_cartUser, $_associatedCreditCards)
+    {   
+        $this->cartUser = $_cartUser;
+        $this->associatedCreditCards = $_associatedCreditCards;
+    }
 }
--- classes/products/product-game.php
+++ classes/products/product-game.php
@@ -7,10 +7,10 @@ class Game extends Product{
     protected $lenght;
     protected $height;
 
-    public function __construct($_brand, $_productName, $_productPrice, $_productCode, $_material, $_length, $_height)
+    public function __construct($_brand, $_productName, $_productPrice, $_productCode, $_materials, $_length, $_height)
     {
         parent::__construct($_brand, $_productName, $_productPrice, $_productCode);
-        $this->material = $_material;
+        $this->materials = $_materials;
         $this->lenght = $_length;
         $this->height = $_height;
     }
